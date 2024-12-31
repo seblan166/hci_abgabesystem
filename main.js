b01163c3539334e7790e26e7d1fcdeab064ed262
vyvez <noah@lohmann-bbg.de>
2024-12-31 14:46:39 +0100

    changed colors and implemented login

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ function addListeners() {
 function showSubmissionContainer(event) {
     event.target.innerHTML += "<div class='submission-container'>Hello World<div>"
 }
+
+function login(){
+    const correct_username = "user"
+    const correct_password = "user"
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    console.log(username)
+    if(username == correct_username && password == correct_password){
+        window.location.href="kurse.html"
+    }else{
+        document.getElementById("wrong-login").style.display = "block"
+    }
+}
