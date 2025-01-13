87c82d451522ec2cf918c4570fe1841696ff9e92
Sebastian <sebastianlangermann@freenet.de>
2025-01-13 11:40:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -387,7 +387,7 @@ function dropFiles(ev) {
     var filesize = files[0].size; 
     if (filesize > 2048){
         console.log(filesize)
-        msg = "Die Datei ist zu groß."
+        msg = "Die Datei ist zu groß. (Maximal 2MB)"
         document.getElementById("submissionDropFieldText").innerText = "Hier bitte Abgabe einfügen. " + msg;
         return
     }
